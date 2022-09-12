7c2aa4d6d67ca9fee9b3e73dcba8689cddc97b27
puranjay <puranjaysingh@gmail.com>
2022-09-12 11:10:21 +0530

    testing testing

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -13,11 +13,7 @@ const Landing = () => {
           <h1>
             Job <span>tracking</span> app
           </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad eius id
-            inventore voluptatum molestiae totam assumenda. Atque exercitationem
-            magnam velit!. Testing out this app.
-          </p>
+          <p>Testing testing</p>
           <button className='btn btn-hero'>Login/Register</button>
         </div>
         <img src={main} alt='job hunt' className='img main-img' />
